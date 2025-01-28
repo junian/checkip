cae50adf90f897d1a081ab6dc26ba2d633326fc3
Junian Triajianto <author@junian.dev>
2025-01-29 02:55:00 +0700

    Init first version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { getConnInfo } from 'hono/cloudflare-workers'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  const info = getConnInfo(c) // info is `ConnInfo`
+  return c.text(`${info.remote.address}\r\n`)
 })
 
 export default app
